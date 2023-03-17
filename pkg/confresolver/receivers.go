5d515c4c9af9162ab0e7f142199dc2133143f5ee
Connor Kelly <connor.kelly@hashicorp.com>
2023-03-17 17:43:30 -0500

    Refactor confresolver helpers
    Each component can only be configured once and we need to be able to
    potentially add a component to multiple pipelines. This commit refactors
    the helpers and inmem confresolver

--- pkg/confresolver/receivers.go
+++ pkg/confresolver/receivers.go
@@ -6,15 +6,38 @@ import "go.opentelemetry.io/collector/component"
 // adds the component to the specified pipeline. It returns the component's
 // configuration for further configuring. The PipelineIDer parameter is the reference
 // returned from NewPipeline
-func (c *Config) NewReceiver(p PipelineIDer, id component.ID) ComponentConfig {
+func (c *Config) NewReceiver(id component.ID, pipelineIDer PipelineIDer, pipelineIDs ...PipelineIDer) ComponentConfig {
 	var ccfg componentConfig
 
 	if c.Receivers == nil {
 		c.Receivers = make(telemetryComponents)
 	}
 
-	pipeline := c.Service.Pipelines[p.id()]
+	pipeline := c.Service.Pipelines[pipelineIDer.id()]
 	pipeline.Receivers, ccfg = addComponent(c.Receivers, id, pipeline.Receivers)
 
+	for _, p := range pipelineIDs {
+		pipeline := c.Service.Pipelines[p.id()]
+		pipeline.Receivers = append(pipeline.Receivers, id)
+	}
+
+	return ccfg
+}
+
+func (c *Config) NewReceivers(id component.ID, pipeline PipelineIDer, p ...PipelineIDer) ComponentConfig {
+	if c.Receivers == nil {
+		c.Receivers = make(telemetryComponents)
+	}
+
+	var ccfg = make(componentConfig)
+	c.Receivers[id] = ccfg
+
+	for _, pipelineID := range p {
+		pipeline := c.Service.Pipelines[pipelineID.id()]
+		if pipeline.Receivers == nil {
+			pipeline.Receivers = make([]component.ID, 0)
+		}
+		pipeline.Receivers = append(pipeline.Receivers, id)
+	}
 	return ccfg
 }
