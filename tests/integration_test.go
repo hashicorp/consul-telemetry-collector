540864f2f0d8df03684ee2409264a36ef7fb916a
Connor Kelly <connor.kelly@hashicorp.com>
2023-10-26 13:53:06 -0500

    Create integration test for otlphttp

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -6,15 +6,26 @@ import (
 	"net"
 	"net/http"
 	"net/http/httptest"
+	"sync/atomic"
 	"testing"
+	"time"
 
+	corev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	metricsv3 "github.com/envoyproxy/go-control-plane/envoy/service/metrics/v3"
+	"github.com/google/uuid"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
-	"github.com/hashicorp/go-hclog"
+	prom "github.com/prometheus/client_model/go"
 	"github.com/shoenig/test/must"
 	"github.com/shoenig/test/portal"
 	otlpcolmetrics "go.opentelemetry.io/proto/otlp/collector/metrics/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/hashicorp/consul-telemetry-collector/internal/otel"
+	"github.com/hashicorp/consul-telemetry-collector/internal/otel/config"
+	"github.com/hashicorp/consul-telemetry-collector/internal/otel/config/helpers/exporters"
+	"github.com/hashicorp/go-hclog"
 )
 
 // impl is an OTLP metrics server.
@@ -101,4 +112,106 @@ func NewTestServer(t *testing.T, validation func(req *otlpcolmetrics.ExportMetri
 		GRPCEndpoint: ln.Addr().String(),
 		HTTPEndpoint: httpSvr.Listener.Addr().String(),
 	}
+}
+
+func Test_OTLPHTTP(t *testing.T) {
+	totalMetric := atomic.Int64{}
+	addrs := NewTestServer(t, func(req *otlpcolmetrics.ExportMetricsServiceRequest) {
+		for _, resourceMetric := range req.GetResourceMetrics() {
+			for _, scopeMetric := range resourceMetric.GetScopeMetrics() {
+				count := int64(len(scopeMetric.GetMetrics()))
+				totalMetric.Add(count)
+			}
+		}
+	})
+	hclog.Default().Info("Running test server", "addr", addrs)
+	collector, err := otel.NewCollector(otel.CollectorCfg{
+		ExporterConfig: &config.ExporterConfig{
+			ID: exporters.BaseOtlpExporterID,
+			Exporter: &exporters.ExporterConfig{
+				Endpoint: fmt.Sprintf("http://%s", addrs.HTTPEndpoint),
+				Headers: map[string]string{
+					"authorization": "abc123",
+				},
+			},
+		},
+	})
+	must.NoError(t, err)
+	ctx := context.Background()
+	go func() { must.NoError(t, collector.Run(ctx)) }()
+
+	total := generateMetrics(t, 30, 30)
+	ch := time.After(30 * time.Second)
+	for {
+		select {
+		case <-ch:
+			t.Logf("Failed to get %d metrics in 30 seconds", total)
+			t.Fail()
+		default:
+			time.Sleep(500 * time.Millisecond)
+		}
+		if totalMetric.Load() == int64(total) {
+			break
+		}
+	}
+
+	collector.Shutdown()
+	hclog.Default().Info("Shutting down")
+}
+
+func ptr[T any](s T) *T {
+	return &s
+}
+
+const counterNumber int = 10
+
+func generateMetrics(t *testing.T, totalSend, metricCount int) (total int) {
+	t.Helper()
+	total = totalSend * metricCount
+	conn, err := grpc.Dial("127.0.0.1:9356", grpc.WithBlock(), grpc.WithInsecure())
+	must.NoError(t, err)
+	client := metricsv3.NewMetricsServiceClient(conn)
+	streamClient, err := client.StreamMetrics(context.Background())
+	must.NoError(t, err)
+
+	for i := 0; i < totalSend; i++ {
+		hclog.Default().Info("sending metric")
+		streamClient.Send(&metricsv3.StreamMetricsMessage{
+			Identifier: &metricsv3.StreamMetricsMessage_Identifier{
+				Node: &corev3.Node{
+					Id:      "integration_test",
+					Cluster: "test",
+				},
+			},
+			EnvoyMetrics: NewEnvoyMetrics(metricCount),
+		})
+	}
+	return total
+}
+
+func NewEnvoyMetrics(metricCount int) []*prom.MetricFamily {
+	metrics := make([]*prom.MetricFamily, metricCount)
+	for i := 0; i < metricCount; i++ {
+		metrics[i] = &prom.MetricFamily{
+			Name:   ptr(uuid.NewString()),
+			Type:   prom.MetricType_COUNTER.Enum(),
+			Metric: NewCounter(counterNumber),
+		}
+	}
+	return metrics
+}
+
+func NewCounter(count int) []*prom.Metric {
+	metrics := make([]*prom.Metric, count)
+	for i := 0; i < count; i++ {
+		metrics[i] = &prom.Metric{
+			Label: []*prom.LabelPair{},
+			Counter: &prom.Counter{
+				Value:            ptr(float64(i)),
+				CreatedTimestamp: timestamppb.Now(),
+			},
+			TimestampMs: ptr(time.Now().UnixMilli()),
+		}
+	}
+	return metrics
 }
