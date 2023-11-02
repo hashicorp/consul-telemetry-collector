5b6905ce04b3fa7ed5ae70d045a2b6aca7e460d6
Connor Kelly <connor.kelly@hashicorp.com>
2023-11-02 12:08:55 -0500

    Add release scripts

---
+++ .release/validate-staging.bash
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+
+SHA=${1}
+if [[ -z $SHA ]]; then
+  echo "Missing build sha"
+  echo "Usage: trigger-promotion.bash SHA"
+fi
+
+set -euo pipefail
+
+source .release/varz.bash
+
+CRT_STAGING_REGISTRY="crt-core-staging-docker-local.artifactory.hashicorp.engineering"
+TARGET=release-default
+
+TAG="${CRT_STAGING_REGISTRY}/${REPO}/${TARGET}:${PRODUCT_VERSION}/${SHA}"
+docker pull $TAG
