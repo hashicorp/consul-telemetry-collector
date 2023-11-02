4a8b08bca229c475057f9d6cb7875f5478335eae
Connor <connor.kelly@hashicorp.com>
2023-11-02 14:44:13 -0500

    Release scripts (#100)

---
+++ .github/scripts/release-staging.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+
+SHA=${1}
+if [[ -z $SHA ]]; then
+  echo "Missing build sha"
+  echo "Usage: release-staging.bash SHA"
+fi
+
+set -euo pipefail
+IFS=$'\n\t'
+
+source .release/varz.bash
+
+bob trigger-promotion \
+  --product-name=${PRODUCT} \
+  --org=${ORG} \
+  --repo=${REPO} \
+  --branch=${BRANCH} \
+  --product-version="${PRODUCT_VERSION}" \
+  --environment=${ENVIRONMENT} \
+  --slack-channel=${SLACK_CHANNEL} \
+  --sha="${SHA}" staging
---
+++ .github/scripts/varz.bash
@@ -0,0 +1,10 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+PRODUCT="consul-telemetry-collector"
+ORG="hashicorp"
+REPO="consul-telemetry-collector"
+BRANCH="main"
+PRODUCT_VERSION=$(cat version/VERSION)
+ENVIRONMENT="consul-telemetry-collector-oss"
+SLACK_CHANNEL="C055APXRUTB"
